fe9a82df8bdd53e0917acdd2a84bfcd0ed2aa2e3
El Cheikh <omar.elcheikh@cgi.com>
2023-10-19 16:27:27 -0400

    fix styling

--- src/model/train.py
+++ src/model/train.py
@@ -55,7 +55,10 @@ def split_data(df):
             'DiabetesPedigree',
             'Age'
         ]].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size=0.30, 
+                                                        random_state=0)
     return X_train, X_test, y_train, y_test
 
 
