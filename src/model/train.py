9607aa5e5be6bbec0ffb52450f6a47899781e9b9
El Cheikh <omar.elcheikh@cgi.com>
2023-10-19 16:25:19 -0400

    fix styling and added linting to pr workflow

--- src/model/train.py
+++ src/model/train.py
@@ -28,7 +28,8 @@ def main(args):
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    model = LogisticRegression(C=reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=reg_rate, solver="liblinear")
+    model.fit(X_train, y_train)
     return model
 
 
@@ -45,16 +46,15 @@ def get_csvs_df(path):
 def split_data(df):
     X, y = df[
         [
-            'Pregnancies', 
-            'PlasmaGlucose', 
-            'DiastolicBloodPressure', 
-            'TricepsThickness', 
-            'SerumInsulin', 
-            'BMI', 
-            'DiabetesPedigree', 
+            'Pregnancies',
+            'PlasmaGlucose',
+            'DiastolicBloodPressure',
+            'TricepsThickness',
+            'SerumInsulin',
+            'BMI',
+            'DiabetesPedigree',
             'Age'
         ]].values, df['Diabetic'].values
-    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
